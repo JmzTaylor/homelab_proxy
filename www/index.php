c8ccf8b3be0cd60ae76766e650657c755df49087
James Taylor <jmz.taylor16@gmail.com>
2021-04-10 10:25:34 -0500

    Fix missing dot

--- www/index.php
+++ www/index.php
@@ -90,7 +90,7 @@ Flight::route('POST /uploadImage', function () {
     if (file_exists($final)) {
         $final = "img/" . $path_parts['filename'] . date("His") . '.' . strtolower($path_parts['extension']);
     } else {
-        $final = "img/" . $path_parts['filename'] . strtolower($path_parts['extension']);
+        $final = "img/" . $path_parts['filename'] . '.' . strtolower($path_parts['extension']);
     }
     move_uploaded_file($file, $final);
 });
